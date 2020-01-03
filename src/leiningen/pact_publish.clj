e931bb22121f52723d7cff27d2b4032b7e7a95b2
Neha Rastogi <neharastogi093@gmail.com>
2020-01-03 23:16:16 +0530

    Use leiningen.core logging instead of println

--- src/leiningen/pact_publish.clj
+++ src/leiningen/pact_publish.clj
@@ -6,16 +6,16 @@
   (filter #(and (.isFile %) (re-find #".*\.json" (.getPath %))) files))
 
 (defn- publish-pacts-to-broker [pact-broker-url pact-directory version]
-  (println "Getting all pact files from " pact-directory)
+  (leiningen.core.main/info (str "INFO: Getting all pact files from " pact-directory))
   (let [pact-file-dir (clojure.java.io/file pact-directory)]
     (if (.exists pact-file-dir)
       (let [error-count (atom 0)
             pact-broker-client (PactBrokerClient. pact-broker-url)
             filtered-pact-files (filter-json-files (file-seq pact-file-dir))]
         (doseq [pact-file filtered-pact-files]
-          (println "Uploading file: " (.getName pact-file))
+          (leiningen.core.main/info (str "INFO: Uploading file: " (.getName pact-file)))
           (let [result (.uploadPactFile pact-broker-client pact-file version)]
-            (println (format "Uploading result '%s' for file '%s'" result (.getName pact-file)))
+            (leiningen.core.main/info (str "INFO: Uploading result '" result "' for file '" (.getName pact-file) "'"))
             (when (.startsWith result "FAILED!")
               (swap! error-count inc))))
         (when (> @error-count 0)
