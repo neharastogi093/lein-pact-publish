9cf29697576ed60e929dbdd8c058e2cfee4c4d62
Neha Rastogi <neharastogi093@gmail.com>
2020-01-06 22:08:53 +0530

    Add scm in project clj

--- project.clj
+++ project.clj
@@ -4,4 +4,6 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[au.com.dius/pact-jvm-consumer "4.0.4" :exclusions [commons-logging]]]
-  :eval-in-leiningen true)
+  :eval-in-leiningen true
+  :scm {:name "git"
+        :url "https://github.com/neharastogi093/lein-pact-publish"})
